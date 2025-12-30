0343cfac6fd3dc1c00ef5df41eec55ca2695f724
p20199 <p20199@unipi.gr>
2025-12-31 00:09:25 +0200

    add double default route

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/antonis.github.io/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
